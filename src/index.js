bbc6566ee9395a6177e5c7c4961b3d4de97f0b5e
Clayton King <clayk@M1-MacBook-Pro.fios-router.home>
2023-05-12 14:57:14 -0400

    Finishing touches, app is now a PWA

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
@@ -21,3 +21,12 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+navigator.serviceWorker
+  .register("./service-worker.js", { scope: "/" })
+  .then(function (registration) {
+    console.log("Service worker registered successfully");
+  })
+  .catch(function (e) {
+    console.error("Error during service worker registration:", e);
+  });
